a96e1841d8934510f995ef765ccaee646c3bc368
kevininga-ga <kevininga813@gmail.com>
2023-07-12 14:09:21 -0400

    fixed bug where the home page wouldn't refresh when i made a campaign and also removed the is active button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,36 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom';
-import { useCombobox } from 'downshift';
-import SingleCampaign from '../components/SingleCampaign';
-import { CampaignContext, CampaignContextProvider } from '../context/campaignContextComponent';
-import categoryURLs from '../data/categoryURLs';
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { useCombobox } from "downshift";
+import SingleCampaign from "../components/SingleCampaign";
+import { CampaignContext } from "../context/campaignContextComponent";
+import { getCampaigns } from "../api/campaignService";
+import categoryURLs from "../data/categoryURLs";
 
 const Home = () => {
-  const { campaigns, donations } = useContext(CampaignContext);
-  const [category, setCategory] = useState('');
+  const { campaigns, setCampaigns } = useContext(CampaignContext);
+  const [category, setCategory] = useState("");
 
   // Function to handle changing the campaign category
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   };
 
+  // Fetch campaigns on component mount
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      try {
+        const data = await getCampaigns();
+        setCampaigns(data);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
+
+    fetchCampaigns();
+  }, []);
+
   // Map the campaigns to an array of titles
-  const items = campaigns.map(campaign => campaign.title);
+  const items = campaigns.map((campaign) => campaign.title);
 
   // Use state to manage the selected item
   const [selectedItem, setSelectedItem] = useState(null);
@@ -34,33 +49,36 @@ const Home = () => {
     items,
     selectedItem,
     onSelectedItemChange: ({ selectedItem }) => {
-      const selectedCampaign = campaigns.find(campaign => campaign.title === selectedItem);
-      setCategory(selectedCampaign?.category || ''); // Set the category of the selected campaign
+      const selectedCampaign = campaigns.find(
+        (campaign) => campaign.title === selectedItem
+      );
+      setCategory(selectedCampaign?.category || ""); // Set the category of the selected campaign
       setSelectedItem(selectedItem); // Set the selected item
     },
     onInputValueChange: ({ inputValue }) => {
-      if (inputValue === '') {
+      if (inputValue === "") {
         setSelectedItem(null); // Clear the selected item when the input is cleared
       }
     },
-    itemToString: (item) => (item ? item : ''), // This function handles the string conversion of each item.
+    itemToString: (item) => (item ? item : ""), // This function handles the string conversion of each item.
   });
 
   // Filter items based on user input
-  const filteredItems = items.filter(item =>
+  const filteredItems = items.filter((item) =>
     item.toLowerCase().includes(inputValue.toLowerCase())
   );
 
   // Filtering campaigns based on selected item and category
-  const filteredCampaigns = campaigns.filter(campaign =>
-    campaign.title.includes(selectedItem || '') &&
-    (category ? campaign.category === category : true)
+  const filteredCampaigns = campaigns.filter(
+    (campaign) =>
+      campaign.title.includes(selectedItem || "") &&
+      (category ? campaign.category === category : true)
   );
-  
+
   // Handles search / category clearing
   const handleReset = () => {
-    setSelectedItem('');
-    setCategory('');
+    setSelectedItem("");
+    setCategory("");
   };
 
   // Downshift functionality ends here
@@ -78,7 +96,8 @@ const Home = () => {
       {/* The dropdown menu */}
       <ul {...getMenuProps()}>
         {/* Show dropdown menu only when it's open and user has typed something */}
-        {isOpen && inputValue &&
+        {isOpen &&
+          inputValue &&
           filteredItems.map((item, index) => {
             const itemIndex = items.indexOf(item); // get the original index of the item
             return (
@@ -87,8 +106,9 @@ const Home = () => {
                 key={item}
                 {...getItemProps({ index: itemIndex, item })}
                 style={{
-                  backgroundColor: highlightedIndex === itemIndex ? 'lightgray' : 'white',
-                  fontWeight: selectedItem === item ? 'bold' : 'normal',
+                  backgroundColor:
+                    highlightedIndex === itemIndex ? "lightgray" : "white",
+                  fontWeight: selectedItem === item ? "bold" : "normal",
                 }}
               >
                 {item}
@@ -107,7 +127,12 @@ const Home = () => {
         ))}
       </select>
 
-      <button onClick={handleReset} className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3  text-white font-bold  hover:text-black mt-4">reset</button>
+      <button
+        onClick={handleReset}
+        className="rounded-full pink-bg lg:w-1/6 md:w-1/6 py-1 w-1/3  text-white font-bold  hover:text-black mt-4"
+      >
+        reset
+      </button>
 
       {/* // Here we render the filtered campaigns */}
       {filteredCampaigns.map((campaign) => (

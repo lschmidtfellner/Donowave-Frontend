453a01eb527fdac6a21f0de72d1734a9b055e060
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-07-12 11:06:57 -0400

    Signup/Signin functionality working tenttatively

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,9 +1,18 @@
 import api from './apiConfig'
 
 export const registerUser = async (registerData) => {
-    const resp = await api.post('/api/register/', registerData);
-    return resp;
-}
+    console.log('Register Data:', registerData); // Log the registerData object
+  
+    try {
+      const resp = await api.post('/api/register/', registerData);
+      console.log('Registration Response:', resp.data); // Log the registration response data
+      return resp;
+    } catch (error) {
+      console.error('Registration Failed:', error); // Log the registration error
+      throw error; // Rethrow the error to handle it in the component
+    }
+  };
+  
   
 export const loginUser = async (loginData) => {
     const resp = await api.post('/api/login/', loginData);
@@ -14,6 +23,15 @@ export const loginUser = async (loginData) => {
     throw new Error('Invalid login credentials');
 }
   
+export const isTokenValid = async () => {
+    try {
+      const response = await api.get('/api/is_token_valid/');
+      return response.data.valid;
+    } catch (error) {
+      throw new Error('Error checking token validity');
+    }
+  };
+
 export const removeToken = () => {
     localStorage.removeItem('authToken');
 }

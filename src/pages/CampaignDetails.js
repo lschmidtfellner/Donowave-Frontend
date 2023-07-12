16c55fa31d7b6e735fc0593c81613c0bd33a4146
lschmidtfellner <lschmidtfellner@gmail.com>
2023-07-12 10:55:24 -0400

    everything up to the donate button is working

--- src/pages/CampaignDetails.js
+++ src/pages/CampaignDetails.js
@@ -1,38 +1,42 @@
-
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import { campaignContextComponent, CampaignContext } from '../context/campaignContextComponent'
+import {
+  campaignContextComponent,
+  CampaignContext
+} from '../context/campaignContextComponent'
 import Swal from 'sweetalert2'
 import categoryURLs from '../data/categoryURLs'
-
-
+import { getCampaign } from '../api/campaignService'
 
 const CampaignDetails = () => {
-  const { campaigns } = useContext(CampaignContext);
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const selectedCampaignId = queryParams.get("id");
-  const [selectedCampaign, setSelectedCampaign] = useState({});
+  const { campaigns } = useContext(CampaignContext)
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const selectedCampaignId = queryParams.get('id')
+  const [selectedCampaign, setSelectedCampaign] = useState({})
 
   useEffect(() => {
-    console.log('Running useEffect', { campaigns, selectedCampaignId });
+    console.log('Running useEffect', { campaigns, selectedCampaignId })
     if (selectedCampaignId) {
-      const campaign = campaigns.find(
-        (campaign) => campaign.id === selectedCampaignId
-      );
-      console.log('Found campaign:', campaign);
-      setSelectedCampaign(campaign);
+      const fetchCampaign = async () => {
+        const campaign = await getCampaign(selectedCampaignId)
+        setSelectedCampaign(campaign)
+      }
+      fetchCampaign()
     }
-  }, [campaigns, selectedCampaignId]);
-  
+  }, [selectedCampaignId])
+
+  // Check if the selectedCampaign object is empty
+  if (Object.keys(selectedCampaign).length === 0) {
+    return <p>Loading...</p>
+  }
 
   return (
     <div>
       <img
         src={
-          categoryURLs.find(
-            (catObj) => catObj.category === selectedCampaign.category
-          ).url
+          categoryURLs.find((catObj) => catObj.category === selectedCampaign.category)
+            .url
         }
       ></img>
       <h2>{selectedCampaign.title}</h2>
@@ -43,7 +47,7 @@ const CampaignDetails = () => {
       <p>{selectedCampaign.description}</p>
       <button>Donate Now</button>
     </div>
-  );
-};
+  )
+}
 
-export default CampaignDetails;
+export default CampaignDetails

8baa180b9c4216ad094b0c0b11a70ce1e629b37d
brooksienyc <cicelybrooks@gmail.com>
2023-07-12 08:10:48 -0400

    cicelyb home page w/ autotype search and category dropdown features - autotype using Downshift

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,105 +1,3 @@
-// import React, { useState, useEffect, useContext } from 'react'
-// import { Link } from 'react-router-dom';
-// import { useCombobox } from 'downshift';
-// import SingleCampaign from '../components/SingleCampaign';
-// import { CampaignContext, CampaignContextProvider } from '../context/campaignContextComponent';
-// import categoryURLs from '../data/categoryURLs';
-
-// const Home = () => {
-//   const { campaigns, donations } = useContext(CampaignContext);
-//   const [category, setCategory] = useState('');
-
-
-//   const handleCategoryChange = (event) => {
-//     setCategory(event.target.value);
-//   };
-
-//   const items = campaigns.map(campaign => campaign.title);
-
-//   const [selectedItem, setSelectedItem] = useState(null);
-
-//   const {
-//     isOpen,
-//     getToggleButtonProps,
-//     getLabelProps,
-//     getMenuProps,
-//     getInputProps,
-//     highlightedIndex,
-//     getItemProps,
-//     inputValue
-//   } = useCombobox({
-//     items,
-//     selectedItem,
-//     onSelectedItemChange: ({ selectedItem }) => {
-//       const selectedCampaign = campaigns.find(campaign => campaign.title === selectedItem);
-//       setCategory(selectedCampaign?.category || '');
-//       setSelectedItem(selectedItem);
-//     },
-//     onInputValueChange: ({ inputValue }) => {
-//       if (inputValue === '') {
-//         setSelectedItem(null);
-//       }
-//     },
-//     itemToString: (item) => (item ? item : ''), // This function handles the string conversion of each item.
-//   });
-
-//   const filteredItems = items.filter(item =>
-//     item.toLowerCase().includes(inputValue.toLowerCase())
-//   );
-
-//   const filteredCampaigns = campaigns.filter(campaign =>
-//     campaign.title.includes(selectedItem || '') &&
-//     (category ? campaign.category === category : true)
-//   );
-
-
-//   return (
-//     <div>
-//       <label {...getLabelProps()}>Search campaigns</label>
-//       <div>
-//         <input {...getInputProps()} />
-//         <button {...getToggleButtonProps()} aria-label="toggle menu" />
-//       </div>
-//       <ul {...getMenuProps()}>
-//         {isOpen && inputValue &&
-//           filteredItems.map((item, index) => {
-//             const itemIndex = items.indexOf(item); // get the original index of the item
-//             return (
-//               <li
-//                 {...getItemProps({ index: itemIndex, item })}
-//                 style={{
-//                   backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
-//                   fontWeight: selectedItem === item ? 'bold' : 'normal',
-//                 }}
-//                 key={item}
-//               >
-//                 {item}
-//               </li>
-//             );
-//           })}
-//       </ul>
-
-
-
-//       <select value={category} onChange={handleCategoryChange}>
-//         <option value="">All Categories</option>
-//         {categoryURLs.map((cat, index) => (
-//           <option key={index} value={cat.category}>
-//             {cat.full_name}
-//           </option>
-//         ))}
-//       </select>
-
-//       {filteredCampaigns.map((campaign) => (
-//         <SingleCampaign key={campaign.id} campaign={campaign} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
 import React, { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom';
 import { useCombobox } from 'downshift';
@@ -172,23 +70,26 @@ const Home = () => {
         <button {...getToggleButtonProps()} aria-label="toggle menu" />
       </div>
 
-      {/* // The dropdown menu */}
+      {/* The dropdown menu */}
       <ul {...getMenuProps()}>
-        {/* // Show dropdown menu only when it's open and user has typed something */}
+        {/* Show dropdown menu only when it's open and user has typed something */}
         {isOpen && inputValue &&
-          filteredItems.map((item, index) => (
-            // Generate each dropdown item
-            <li
-              key={item}
-              {...getItemProps({ index, item })}
-              style={{
-                backgroundColor: highlightedIndex === index ? 'lightgray' : 'white',
-                fontWeight: selectedItem === item ? 'bold' : 'normal',
-              }}
-            >
-              {item}
-            </li>
-          ))}
+          filteredItems.map((item, index) => {
+            const itemIndex = items.indexOf(item); // get the original index of the item
+            return (
+              // Generate each dropdown item
+              <li
+                key={item}
+                {...getItemProps({ index: itemIndex, item })}
+                style={{
+                  backgroundColor: highlightedIndex === itemIndex ? 'lightgray' : 'white',
+                  fontWeight: selectedItem === item ? 'bold' : 'normal',
+                }}
+              >
+                {item}
+              </li>
+            );
+          })}
       </ul>
 
       {/* // The category filter dropdown */}

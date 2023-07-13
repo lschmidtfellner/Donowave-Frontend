86f8fec1798e5b735472cadaa276909d9ce22907
kevininga-ga <kevininga813@gmail.com>
2023-07-13 15:00:50 -0400

    added refirect to campaign details from my account when clicking on a capmaign:

--- src/components/SingleCampaign.js
+++ src/components/SingleCampaign.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import categoryURLs from '../data/categoryURLs'
-import { Link } from 'react-router-dom'
-import dateInterpreter from '../data/dateInterpreter'
+import React from "react";
+import categoryURLs from "../data/categoryURLs";
+import { Link } from "react-router-dom";
+import dateInterpreter from "../data/dateInterpreter";
 
 const SingleCampaign = ({ campaign }) => (
   <Link to={`/campaigns/details?id=${campaign.id}`}>
@@ -15,10 +15,11 @@ const SingleCampaign = ({ campaign }) => (
       <h2>{campaign.title}</h2>
       <p>{dateInterpreter(campaign.deadline)}</p>
       <p>
-        {Number(campaign.raised_amount).toLocaleString()}DC of {Number(campaign.goal_amount).toLocaleString()}DC
+        {Number(campaign.raised_amount).toLocaleString()} DC of{" "}
+        {Number(campaign.goal_amount).toLocaleString()} DC
       </p>
     </div>
   </Link>
-)
+);
 
-export default SingleCampaign
+export default SingleCampaign;

8ae29cec3bc799bcc7a1e983e1b4e9e14d410bf6
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-07-14 20:09:23 -0400

    again

--- src/interfaces/ERC20Interface.js
+++ src/interfaces/ERC20Interface.js
@@ -1,24 +1,26 @@
-import { Alchemy, Network } from 'alchemy-sdk'
-import { ethers } from 'ethers'
-import erc20ContractABI from '../contracts/ABIs/erc20ABI.js'
+import { ethers } from 'ethers';
+import erc20ContractABI from '../contracts/ABIs/erc20ABI.js';
 import connectToContract from '../util/contractConnection.js';
 
-// Optional config object, but defaults to the API key 'demo' and Network 'eth-mainnet'.
+// Your alchemy setup
 const alchemySettings = {
     apiKey: process.env.REACT_APP_API_KEY,
     network: Network.ETH_SEPOLIA 
 };
 
-const alchemy = new Alchemy(alchemySettings)
-const nftAddress = process.env.REACT_APP_ERC20_CONTRACT_ADDRESS
-const provider = new ethers.BrowserProvider(window.ethereum)
-const erc20signer = await provider.getSigner()
-console.log(nftAddress, erc20ContractABI, erc20signer)
+const alchemy = new Alchemy(alchemySettings);
+const nftAddress = process.env.REACT_APP_ERC20_CONTRACT_ADDRESS;
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const erc20signer = provider.getSigner();
+console.log(nftAddress, erc20ContractABI, erc20signer);
 // erc20 contract
-export const erc20contract = connectToContract(nftAddress, erc20ContractABI, erc20signer)
-console.log(erc20contract)
+export const erc20contract = connectToContract(nftAddress, erc20ContractABI, erc20signer);
+console.log(erc20contract);
 
 export const tokenTransfer = async (recipientAddress, amount) =>  {
-    const transaction = await erc20contract.transfer(recipientAddress, amount)
-    return transaction
-}
+    // Convert the amount to BigNumber, and then to the smallest unit of the token (often called "wei")
+    const amountInWei = ethers.utils.parseEther(amount);
+
+    const transaction = await erc20contract.transfer(recipientAddress, amountInWei);
+    return transaction;
+};

4d7c007fa29ae3ac89eebc37a29cad723ef9b5ee
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-07-14 20:23:53 -0400

    again

--- src/api/sendEther.js
+++ src/api/sendEther.js
@@ -1,5 +1,5 @@
-import { ethers, utils } from 'ethers';
-import erc20ABI from '../contracts/ABIs/erc20ABI'; // adjust the path to point to your erc20ABI.js file
+import { ethers } from 'ethers';
+import erc20ABI from '../contracts/ABIs/erc20ABI'; 
 
 export const sendToken = async (provider, signer, amount, recipient) => {
     if (!provider || !signer) {
@@ -10,7 +10,7 @@ export const sendToken = async (provider, signer, amount, recipient) => {
     const tokenContractAddress = process.env.REACT_APP_ERC20_CONTRACT_ADDRESS;
 
     // Convert the amount to the smallest unit of the token (often called "wei")
-    const amountInWei = utils.parseEther(amount);
+    const amountInWei = ethers.utils.parseEther(amount);
 
     // Create a contract instance
     const contract = new ethers.Contract(tokenContractAddress, erc20ABI, signer);

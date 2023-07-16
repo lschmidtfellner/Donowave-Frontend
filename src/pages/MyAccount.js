2a04c10857a32602c849c4e9ae16e1dcbb256551
brooksienyc <cicelybrooks@gmail.com>
2023-07-16 18:26:30 -0400

    cicelyb worked on MyAccount edit settings added

--- src/pages/MyAccount.js
+++ src/pages/MyAccount.js
@@ -8,6 +8,7 @@ function MyAccount() {
   const { user } = useContext(AuthContext);
   const [userCampaigns, setUserCampaigns] = useState([]);
   const [userDonations, setUserDonations] = useState([]);
+  const [password, setPassword] = useState('');
 
   useEffect(() => {
     const fetchUserCampaignsAndDonations = async () => {
@@ -31,6 +32,17 @@ function MyAccount() {
         <h2 className="text-left text-3xl font-bold leading-9 tracking-tight text-black mb-10">
             MY SETTINGS
           </h2>
+          <div className="bg-white rounded-md mb-8 p-4 shadow">
+          <label className="font-bold text-xs text-left">EDIT PASSWORD:</label>
+          <input
+            className="block w-full rounded-full border-0 py-1.5 pl-5 pr-5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-1 mb-6"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="aqua rounded-full lg:w-1/6 md:w-1/6 py-2 w-1/2 text-white font-bold  hover:text-black mt-4 text-xs">Submit</button>
+          </div>
           <div className="border-t-2 border-gray-400 my-10 w-1/2"></div>
           <h2 className="text-left text-3xl font-bold leading-9 tracking-tight text-black mb-10">
             CAMPAIGNS
